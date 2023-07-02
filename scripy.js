713611d22e13e3465209c171cd1f2a552aa99bce
shakhmurad <muradb356@gmail.com>
2023-07-02 15:07:32 +0400

    add responsiv width:700px

---
+++ scripy.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        nav:true,
+        loop:true,
+        autoplay:false,
+        autoplayTimeout:5000,
+        
+
+    });
+  });
